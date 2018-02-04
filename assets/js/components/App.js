6f0fcf570c98cc4dc250b0730ae3d538413b839d
hays.hopkins@salesloft.com <hays.hopkins@salesloft.com>
2018-02-04 12:17:11 -0500

    Hello world react via phx api

--- assets/js/components/App.js
+++ assets/js/components/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      message: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch('/api/hello').then((response) => {
+      return response.json();
+    }).then((body) => {
+      this.setState({message: body.data});
+    });
+  }
+
   render() {
     return(
-      <div>Hello, world!</div>
+      <div>{this.state.message}</div>
     );
   }
 }
